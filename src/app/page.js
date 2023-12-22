11a4c8c13193f669d20a6476d7149ae7c9fec7d9
anurag.pandey17 <anurag.pandey17@gmail.com>
2023-12-22 10:17:59 +0530

    issue reslove

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,8 @@
-import BackgroundChanger from '@/component/Background'
-import Hero from '@/component/hero'
 import New from '@/component/new'
 
 export default function Home() {
   return (
 <>
-{/* <Header/> */}
-{/* <Hero /> */}
 <New/>
 </>
   )
