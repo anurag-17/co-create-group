a75f889a45e5126c4ac1cec56b30de01c6b4bb58
anurag.pandey17 <anurag.pandey17@gmail.com>
2024-01-08 14:11:46 +0530

    change baseurl

--- src/component/config.jsx
+++ src/component/config.jsx
@@ -1,2 +1,2 @@
-export const BASE_URL = "https://co-create-group-admin-panel.vercel.app"
-// export const BASE_URL = "http://54.80.63.9:4000"
+// export const BASE_URL = "https://co-create-group-admin-panel.vercel.app"
+export const BASE_URL = "http://18.213.58.52:5000"
