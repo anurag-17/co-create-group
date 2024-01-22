74c7e95dc18e4b656117d633be9d1a9510e80b61
anurag.pandey17 <anurag.pandey17@gmail.com>
2024-01-22 17:23:46 +0530

    changes

--- src/component/config.jsx
+++ src/component/config.jsx
@@ -1,3 +1,3 @@
 // export const BASE_URL = "https://co-create-group-admin-panel.vercel.app"
-// export const BASE_URL = "http://18.213.58.52:5000"
-export const BASE_URL = "http://localhost:5000"
+export const BASE_URL = "http://18.213.58.52:5000"
+// export const BASE_URL = "http://localhost:5000"
